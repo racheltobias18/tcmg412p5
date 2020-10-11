67d659424cc01ef744f65785fab9f33e1bb9ff22
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 17:52:19 -0500

    Set up bare-bones Flask App

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 from flask import Flask
-import os
-import time
-import re
-from slackclient import SlackClient
 
 DEBUG = True
-
 app = Flask(__name__)
 slack_app = None
 slack_app_id = None
+
+# Flask Methods
+@app.route("/md5")
+def calc_md5(data_to_hash):
+    return ""
+
+@app.route("/factorial")
+def calc_factorial(number):
+    return 0
+
+@app.route("/fibonacci")
+def calc_fibonacci(number):
+    return 0
+
+@app.route("/is-prime")
+def calc_is_prime(number):
+    return False
+
+@app.route("/slack-alert")
+def post_slack_alert(message):
+    return False
 
 if __name__ == "__main__":
     SLACK_KEY = None
     for l in open("slack.key"):
         SLACK_KEY = l
-    slack_app = SlackClient(SLACK_KEY)
-    if slack_app.rtm_connect(with_team_state=False):
-        slack_app_id = slack_app.api_call("auth.test")["user_id"]
-        print("Slack Bot connected...")
-    else:
-        print("Slack Bot could not connect!")
-        if DEBUG == False:
-            exit(1)
     app.run(host="0.0.0.0")
