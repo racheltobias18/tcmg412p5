965017332295c01dc21a306bde7ee2379f1aebf0
Trent Spears <spears.trent@gmail.com>
2020-10-11 18:49:53 -0500

    Set up Slack Alert function

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask, Response, jsonify
+import os
+import re
+from slack import WebClient
 
 DEBUG = True
 app = Flask(__name__)
@@ -29,11 +32,26 @@ def calc_is_prime(number):
 
 @app.route("/slack-alert/<string:message>")
 def post_slack_alert(message):
-    response = False
-    return jsonify(input=message, output=response)
+    response = slack_app.chat_postMessage(channel='#group-4-app', text=message, user="Group4 Alert Bot")
+    return jsonify(input=message, output=response["ok"])
 
 if __name__ == "__main__":
+    print("Attempting to read Slack App Key from slack.key file...")
     SLACK_KEY = None
     for l in open("slack.key"):
         SLACK_KEY = l
+    if SLACK_KEY == None or len(SLACK_KEY) <= 0:
+        print("ERROR: Could not read Slack App Key from slack.key file!")
+        if DEBUG == False:
+            exit(1)
+    else:
+        slack_app = WebClient(SLACK_KEY)
+        if slack_app.rtm_connect(with_team_state=False):
+            slack_app_id = slack_app.api_call("auth.test")["user_id"]
+            print("Slack App connected with ID: ", slack_app_id)
+        else:
+            print("ERROR: Could not connect App to Slack!")
+            if DEBUG == False:
+                exit(1)
+    print("Launching Flask App.")
     app.run(host="0.0.0.0")
