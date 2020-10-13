1ba8b237634d2967cd9cd0633eaeab5913c34927
vagrant <vagrant@ubuntu1804.localdomain>
2020-10-13 08:15:09 +0000

    Fibonacci v1.0

--- app.py
+++ app.py
@@ -18,7 +18,28 @@ def calc_factorial(number):
 
 @FLASK_APP.route("/fibonacci/<int:number>")
 def calc_fibonacci(number):
-    fibonacci = 0
+    
+    fibonacci = []
+    c1 = 0
+    c2 = 1
+    fib = 0
+    check = 0
+    
+    if number < 0:
+        fibonacci = "Error: Please use a number greater or equal to 0"
+    
+    elif number == 0:
+        fibonacci = 0
+    
+    else:
+        while check == 0:
+            fib = c1 + c2
+            c2 = c1
+            c1 = fib
+            if fib <= number:
+                fibonacci.append(fib)
+            else:
+                check = 1
     return jsonify(input=number, output=fibonacci)
 
 @FLASK_APP.route("/is-prime/<int:number>")
